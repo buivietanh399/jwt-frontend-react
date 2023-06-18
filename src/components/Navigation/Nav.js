9bcd9c55b08f7fe50667558e872cc77d5355bfdf
Bui Viet Anh <buivietanh399@gmail.com>
2023-06-18 21:46:01 +0700

    init project

---
+++ src/components/Navigation/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Nav.scss";
+const Nav = (props) => {
+  return (
+    <div>
+      <div class="topnav">
+        <a class="active" href="#home">
+          Home
+        </a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
